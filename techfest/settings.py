74997dce1a9d5b77e359d281a93cb20e99287222
SRINIVAS <srinivasacademics@gmail.com>
2025-07-22 17:17:02 +0530

    changed allowed host

--- techfest/settings.py
+++ techfest/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dbmsdeplaoylive.onrender.com', 'localhost', '127.0.0.1']
 
 
 
