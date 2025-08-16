4ad4e26beb4177d020278079391eedbfe12a1639
SRINIVAS <srinivasacademics@gmail.com>
2025-08-16 15:31:08 +0530

    Update urls.py

--- techfest/urls.py
+++ techfest/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from festapp.views import health_check
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('health/', health_check),
     path('', include('festapp.urls')),  # 👈 Add this line
 ]
 
